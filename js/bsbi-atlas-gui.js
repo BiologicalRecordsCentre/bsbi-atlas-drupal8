5aa99a5426c6c2a2e4ede8e78ab28d9c85f098b8
Richard Burkmar <rburkmar@ceh.ac.uk>
2020-11-16 12:21:10 +0000

    Doubled dummy para's on empty sections

--- js/bsbi-atlas-gui.js
+++ js/bsbi-atlas-gui.js
@@ -215,7 +215,9 @@ var bsbiDataRoot
   }
 
   function sectionEmpty(id, tabs) {
-    $sect = $('#bsbi-atlas-section-' + id)
+    var $sect = $('#bsbi-atlas-section-' + id)
+    var $p = $('<p>').appendTo($sect)
+    $p.text('Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.')
     $p = $('<p>').appendTo($sect)
     $p.text('Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.')
     sectionEnd($sect, tabs)
