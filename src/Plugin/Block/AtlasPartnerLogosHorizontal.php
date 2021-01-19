b67b5e0be6b70c2f33be0a2cc2b5eed90cb36292
Rich Burkmar <rich.burkmar@gmail.com>
2021-01-19 12:30:41 +0000

    Removed title from main bar and added footer image block

---
+++ src/Plugin/Block/AtlasPartnerLogosHorizontal.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Drupal\bsbi_atlas\Plugin\Block;
+
+use Drupal\Core\Block\BlockBase;
+use Drupal\Core\Url;
+
+/**
+ * Provides a Block for Partner logos.
+ *
+ * @Block(
+ *   id = "atlas_partner_logos_horizontal",
+ *   admin_label = @Translation("Atlas partner logos (horizontal)"),
+ *   category = @Translation("BSBI Atlas"),
+ * )
+ */
+class AtlasPartnerLogosHorizontal extends BlockBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function build() {
+
+    $base_path = Url::fromRoute('<front>', [], ['absolute' => TRUE])->toString();
+
+    $bsbi = $base_path . drupal_get_path('module', 'bsbi_atlas') . '/images/BSBI-logo.gif';
+    $ceh = $base_path . drupal_get_path('module', 'bsbi_atlas') . '/images/UKCEH-logo.png';
+    $brc = $base_path . drupal_get_path('module', 'bsbi_atlas') . '/images/BRC-logo.png';
+
+    $html = '<img src="' . $bsbi . '" class="partner-logo-horizontal" />';
+    $html .= '<img src="' . $ceh . '" class="partner-logo-horizontal" />';
+    $html .= '<img src="' . $brc . '" class="partner-logo-horizontal" />';
+
+    return [
+      '#markup' => $html,
+    ];
+  }
+
+}
